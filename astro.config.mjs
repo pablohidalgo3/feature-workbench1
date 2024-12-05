818c137c34584147efc615a3b9a6ca2c2aee80b1
Pablo Hidalgo Prados <pablohidalgo3@gmail.com>
2024-12-05 19:31:36 +0100

    Inicio renovación web

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  output: 'server',
+  integrations: [tailwind()],
+
+  env: {
+    schema: {
+      PLAYERS_API_ENDPOINT: envField.string({ context: 'server', access: 'public' }),
+    }
+  },
+  adapter: vercel()
 });
